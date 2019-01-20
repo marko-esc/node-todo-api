0175ffd66e83c2030c15f10c2d54c0ce04f28a09
Mark O'Connor <lemoco@hotmail.ca>
2019-01-20 13:17:03 -0500

    Add test for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,11 +4,20 @@ const request = require('supertest');
 const {app} = require('../server');
 const {Todo} = require('../models/todo');
 
+const todos = [
+    {text: 'Test todo seed'},
+    {text: 'Second test todo'}
+];
+
 beforeEach((done) => {
     Todo.remove({}).then(() => {
-        done();
+        return Todo.insertMany(todos);
+    })
+    .then(()=> {
+        return done();
     });
 });
+
 
 describe ('POST/todos', () => {
     it('should create a new todo', (done) => {
@@ -26,7 +35,7 @@ describe ('POST/todos', () => {
                    return done(err);
                 }
 
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -46,11 +55,23 @@ describe ('POST/todos', () => {
                 }
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => {
                     done(e);
                 })
             })
+    })
+});
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done());
     })
 });
