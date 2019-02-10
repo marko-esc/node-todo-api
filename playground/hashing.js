e3535540dc5f1d2b55b45c4204ed3a30801f8bd8
Mark O'Connor <lemoco@hotmail.ca>
2019-02-10 12:39:31 -0500

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,29 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = 'abc123';
 
-var token = jwt.sign(data, 'abc123');
-console.log(token);
-var decoded = jwt.verify(token, 'abc123');
-console.log(decoded);
+// bcrypt.genSalt(10, (err, salt) =>{
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     })
+// });
+
+var hashedPassword = '$2a$10$j06i06i4J0OD08v5hmkAvejz6s8Bi.pgk./0Bnr9UcJ4k.8dalBcC';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+})
+
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, 'abc123');
+// console.log(token);
+// var decoded = jwt.verify(token, 'abc123');
+// console.log(decoded);
 
 
 // var text = 'A test string of some characters';
